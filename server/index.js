0d2d9b3be07a27526d1f7088b0c4ce57f12d73be
Govind Penshanwar <govindpenshanwar32@gmail.com>
2024-02-13 20:27:52 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,101 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+require('./dbConfig/dbConfig.js')
+const Todos = require('./models/model.js');
+const cors = require('cors')
+
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+//middleWares 
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/todos", async (req, res) => {
+    try {
+
+        const todos = await Todos.find();
+        return res.send({
+            // success : true,
+            todos
+        })
+
+    } catch (error) {
+        return res.json({
+            success: false,
+            error: error.message
+        })
+    }
+});
+
+app.post('/api/todos', async (req, res) => {
+    const { name, description } = req.body;
+    try {
+        const newTodo = await Todos.create({
+            name,
+            description
+        });
+
+        return res.json({
+            success: true,
+            newTodo
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+            error: error.message
+        })
+    }
+});
+
+app.put('/api/todos/:id', async (req, res) => {
+    const todoId = req.params.id;
+    const { name, description } = req.body;
+
+    try {
+        const updatedTodo = await Todos.findByIdAndUpdate(todoId, { name, description }, { new: true });
+        if (!updatedTodo) {
+            return res.status(404).json({
+                success: false,
+                err: "Todo not Found"
+            })
+        };
+
+        return res.status(200).json({
+            success: true,
+            updatedTodo
+        })
+
+    } catch (error) {
+        console.error("Error updating Todo at put route => ", error.message);
+    }
+})
+
+app.delete('/api/todos/:id', async (req, res) => {
+    const todoId = req.params.id;
+    try {
+        const deletedTodo = await Todos.findByIdAndDelete(todoId);
+        if (!deletedTodo) {
+            return res.status(404).json({
+                success: false,
+                error: 'Todo not found'
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            deletedTodo
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
+})
+
+
+app.listen(port, () => {
+    console.log(`Server Started  at port ${port}`);
+});
