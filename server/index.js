26a631090c554e3220b78ce0549909245dcf711b
Govind Penshanwar <govindpenshanwar32@gmail.com>
2024-03-29 00:59:38 +0530

    fixed issues

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8080;
 //middleWares 
 app.use(express.json());
 app.use(cors({
-    origin: '*'
+    origin: 'https://task-client-pearl.vercel.app'
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
